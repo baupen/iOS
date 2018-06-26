d94bdba78c40d2c53dd8b82e36f972840cebfd0e
juliand665 <julian.dunskus@gmail.com>
2018-06-26 03:43:42 +0200

    updated to new API spec

--- Issue Manager/Back End/JSend.swift
+++ Issue Manager/Back End/JSend.swift
@@ -16,12 +16,11 @@ struct JSendError: Decodable {
 }
 
 enum APIError: Int, Error, Decodable {
-	case emptyRequest = 0
 	/// something in the request was malformed, e.g. missing value in json or missing image in multipart
 	case invalidRequest = 1
 	/// the authentication token is invalid, e.g. because it has expired
 	case invalidToken = 2
-	/// the client is outdated, i.e. rejected by the server
+	/// the client is outdated, thus the server won't risk communicating with it
 	case outdatedClient = 3
 	
 	case unknownUsername = 100
@@ -33,4 +32,6 @@ enum APIError: Int, Error, Decodable {
 	case outdatedData = 202
 	/// whatever the client is trying to do is impossible, e.g. reviewing a closed issue
 	case invalidAction = 203
+	/// an unknown error occurred on the server
+	case unknownError = 300
 }
