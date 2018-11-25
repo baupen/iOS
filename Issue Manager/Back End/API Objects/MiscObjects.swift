b385ae3a98bfedacb1a222f1fceb93cf191cc150
Julian Dunskus <julian.dunskus@gmail.com>
2018-11-25 04:46:58 +0100

    added color en-/decoding logic
    forgot this earlier… oops

--- Issue Manager/Back End/API Objects/MiscObjects.swift
+++ Issue Manager/Back End/API Objects/MiscObjects.swift
@@ -11,4 +11,26 @@ struct Color: Codable {
 	var red: UInt8
 	var green: UInt8
 	var blue: UInt8
+	
+	init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		let raw = try container.decode(String.self)
+		guard
+			raw.count == 7,
+			raw.hasPrefix("#"),
+			let int = Int(raw.dropFirst(), radix: 16)
+			else { throw DecodingError.dataCorruptedError(in: container, debugDescription: "Expecting hex string #RRGGBB, got '\(raw)'") }
+		
+		red = UInt8(int >> 16 & 0xFF)
+		green = UInt8(int >> 8 & 0xFF)
+		blue = UInt8(int >> 0 & 0xFF)
+	}
+	
+	func encode(to encoder: Encoder) throws {
+		var container = encoder.singleValueContainer()
+		
+		let int = red << 16 | green << 8 | blue << 0
+		let raw = "#" + String(int, radix: 16, uppercase: true)
+		try container.encode(raw)
+	}
 }
