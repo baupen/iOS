8097b4294cf349c582758685aa752d255d481649
Julian Dunskus <julian.dunskus@gmail.com>
2019-05-12 00:41:19 +0200

    improved "stay logged in" logic
    was a little shaky before

--- Issue Manager/Issue Manager/Login/LoginViewController.swift
+++ Issue Manager/Issue Manager/Login/LoginViewController.swift
@@ -51,21 +51,23 @@ final class LoginViewController: LoginHandlerViewController {
 		usernameField.delegate = self
 		passwordField.delegate = self
 		
+		stayLoggedInSwitch.isOn = defaults.stayLoggedIn
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
 		if let localUser = Client.shared.localUser, !localUser.localUsername.isEmpty {
 			usernameField.text = localUser.localUsername
 			
 			if defaults.stayLoggedIn, !localUser.hasLoggedOut {
-				DispatchQueue.main.async {
-					self.showSiteList(userInitiated: false)
-				}
+				showSiteList(userInitiated: false)
 			} else {
 				passwordField.becomeFirstResponder()
 			}
 		} else {
 			usernameField.becomeFirstResponder()
 		}
-		
-		stayLoggedInSwitch.isOn = defaults.stayLoggedIn
 	}
 	
 	func logIn() {
