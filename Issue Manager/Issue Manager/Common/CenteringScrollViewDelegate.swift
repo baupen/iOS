0cf41fb412609cc8717bbcbf3dfd220cff93dec4
Julian Dunskus <julian.dunskus@gmail.com>
2019-11-27 00:15:13 +0100

    centering fix
    this broke in iOS 13

--- Issue Manager/Issue Manager/Common/CenteringScrollViewDelegate.swift
+++ Issue Manager/Issue Manager/Common/CenteringScrollViewDelegate.swift
@@ -5,16 +5,33 @@ import UIKit
 final class CenteringScrollViewDelegate: NSObject, UIScrollViewDelegate {
 	@IBOutlet private weak var viewForZooming: UIView?
 	
+	private var boundsObservation: NSKeyValueObservation?
+	
 	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
 		centerContent(of: scrollView) // initial setup, if needed
 		return viewForZooming
+	}
+	
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		centerContent(of: scrollView)
 	}
 	
 	func scrollViewDidZoom(_ scrollView: UIScrollView) {
 		centerContent(of: scrollView)
 	}
 	
+	func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) {
+		centerContent(of: scrollView)
+	}
+	
 	func centerContent(of scrollView: UIScrollView) {
+		if boundsObservation == nil {
+			// need to observe bounds (at least in iOS 13) because we're not guaranteed to get a call to anything on bounds change
+			boundsObservation = scrollView.observe(\.bounds) { [unowned self] scrollView, change in
+				self.centerContent(of: scrollView)
+			}
+		}
+		
 		let offset = 0.5 * (scrollView.bounds.size - scrollView.contentSize).map { max(0, $0) }
 		scrollView.contentInset = UIEdgeInsets(top: offset.y, left: offset.x, bottom: 0, right: 0)
 	}
