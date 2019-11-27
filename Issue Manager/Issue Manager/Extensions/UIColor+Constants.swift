86ad8e10902535f0cc01b1e4d24714d6ee55bccf
Julian Dunskus <julian.dunskus@gmail.com>
2019-11-27 03:47:13 +0100

    adjusted dark mode map background
    it's now a softer-on-the-eyes 75% white, leaving everything readable while not blinding you

--- Issue Manager/Issue Manager/Extensions/UIColor+Constants.swift
+++ Issue Manager/Issue Manager/Extensions/UIColor+Constants.swift
@@ -7,4 +7,6 @@ extension UIColor {
 	static let attention = UIColor(named: "Attention")!
 	static let unimportant = UIColor(named: "Unimportant")!
 	static let clientMode = UIColor(named: "Client Mode")!
+	static let splashBackground = UIColor(named: "Splash Background")!
+	static let mapBackground = UIColor(named: "Map Background")!
 }
