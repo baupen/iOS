d69910e6b778b9bc1ad7fe12a85d26951e6b4afa
Julian Dunskus <julian.dunskus@gmail.com>
2019-12-10 04:06:39 +0100

    fixed site list layout not respecting safe area

--- Issue Manager/Issue Manager/Construction Site List/SiteListLayout.swift
+++ Issue Manager/Issue Manager/Construction Site List/SiteListLayout.swift
@@ -8,7 +8,10 @@ final class SiteListLayout: UICollectionViewFlowLayout {
 		
 		guard let collectionView = collectionView else { return }
 		
-		let availableSize = collectionView.bounds.inset(by: sectionInset).size
+		let availableSize = collectionView.bounds
+			.inset(by: collectionView.adjustedContentInset)
+			.inset(by: sectionInset)
+			.size
 		let minimumWidth: CGFloat = 300
 		let spacing = minimumLineSpacing
 		let amount = max(1, ((availableSize.width + spacing) / (minimumWidth + spacing)).rounded(.down))
@@ -18,18 +21,18 @@ final class SiteListLayout: UICollectionViewFlowLayout {
 	
 	// snaps cells to bounds; relies on the layout being a single horizontally scrolling list
 	override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-		guard proposedContentOffset.x > 0 else { return proposedContentOffset }
+		let leftInset = collectionView!.adjustedContentInset.left
+		let proposedX = proposedContentOffset.x + leftInset
+		guard proposedX > 0 else { return proposedContentOffset }
 		
 		let offset = minimumLineSpacing / 2
 		let lineDistance = itemSize.width + minimumLineSpacing
-		let offsetWithinCell = proposedContentOffset.x.truncatingRemainder(dividingBy: lineDistance) - offset
+		let offsetWithinCell = proposedX.truncatingRemainder(dividingBy: lineDistance) - offset
 		let rounded = (offsetWithinCell / lineDistance).rounded() * lineDistance
-		let offsetWithinView = (proposedContentOffset.x / lineDistance).rounded(.down) * lineDistance
-		let targetOffset = offsetWithinView + rounded
+		let offsetWithinView = (proposedX / lineDistance).rounded(.down) * lineDistance
+		let targetOffset = offsetWithinView + rounded - leftInset
 		return CGPoint(x: targetOffset, y: proposedContentOffset.y)
 	}
 	
-	override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
-		true
-	}
+	override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool { true }
 }
