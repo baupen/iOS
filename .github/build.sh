a6185b86cc7ed999f012070957f05a37c785f6fa
Julian Dunskus <julian.dunskus@gmail.com>
2021-03-13 04:18:43 +0100

    still trying to fully disable code signing

--- .github/build.sh
+++ .github/build.sh
@@ -5,4 +5,5 @@ xcodebuild clean build \
 	-project "Issue Manager/Issue Manager.xcodeproj" \
 	-scheme "Issue Manager" \
 	CODE_SIGN_IDENTITY="" \
-	CODE_SIGNING_REQUIRED=NO
+	CODE_SIGNING_REQUIRED=NO \
+	CODE_SIGNING_ALLOWED=NO
